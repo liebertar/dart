c8a1043f07ffff712ae9a9a7e9db556b9ba4f7dd
wolverine@wondermove.net <wolverine@wondermove.net>
2024-02-24 16:40:26 +0900

    first commit Dart

---
+++ dynamicType.dart
@@ -0,0 +1,7 @@
+void main() {
+    dynamic name = 'ck';
+    name = 1;
+    name = 0.98234972983749;
+    name = true;
+    print(name);
+}
---
+++ var.dart
@@ -0,0 +1,4 @@
+void main() {
+    String name = 'ck';
+    print(name);
+}
